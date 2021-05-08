7470f43c25cf42e13b9cc8903839770f605cc33a
Pehra <pookiepehrson@gmail.com>
2021-05-08 02:56:58 -0700

    outlined code in TempSens.h

---
+++ TempSensMaster/TempSensorLib/src/TempSens.cpp
@@ -0,0 +1,11 @@
+/******************************
+	This librry is for the hands free temperature sensor project
+	
+	Authors:
+		Kaleb Krieger 
+		Andrew Pehrson 
+		Quentin Onyemordi
+		
+******************************/
+
+#include "TempSems.h"
