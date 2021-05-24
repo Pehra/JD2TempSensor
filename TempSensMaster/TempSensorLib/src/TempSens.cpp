f680f88d99868c18311b626e4be27a811284683f
Andrew Pehrson <35977942+Pehra@users.noreply.github.com>
2021-05-23 22:46:23 -0700

    Update TempSens.cpp

--- TempSensMaster/TempSensorLib/src/TempSens.cpp
+++ TempSensMaster/TempSensorLib/src/TempSens.cpp
@@ -304,5 +304,61 @@ void TempSens::testBuzer(){
  * Function that tests the oled
  *******************************************************************************/
 void TempSens::testOled(){
-	
+	static const unsigned char PROGMEM logo_bmp[] =
+	{ 0b00000000, 0b11000000,
+	  0b00000001, 0b11000000,
+	  0b00000001, 0b11000000,
+	  0b00000011, 0b11100000,
+	  0b11110011, 0b11100000,
+	  0b11111110, 0b11111000,
+	  0b01111110, 0b11111111,
+	  0b00110011, 0b10011111,
+	  0b00011111, 0b11111100,
+	  0b00001101, 0b01110000,
+	  0b00011011, 0b10100000,
+	  0b00111111, 0b11100000,
+	  0b00111111, 0b11110000,
+	  0b01111100, 0b11110000,
+	  0b01110000, 0b01110000,
+	  0b00000000, 0b00110000 };
+	uint8_t w = 16;
+	uint8_t h = 16;
+	int8_t f, icons[NUMFLAKES][3];
+
+	// Initialize 'snowflake' positions
+	for(f=0; f< NUMFLAKES; f++) {
+		icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+		icons[f][YPOS]   = -LOGO_HEIGHT;
+		icons[f][DELTAY] = random(1, 6);
+		Serial.print(F("x: "));
+		Serial.print(icons[f][XPOS], DEC);
+		Serial.print(F(" y: "));
+		Serial.print(icons[f][YPOS], DEC);
+		Serial.print(F(" dy: "));
+		Serial.println(icons[f][DELTAY], DEC);
+	}
+
+  	for(;;) { // Loop forever...
+    		display.clearDisplay(); // Clear the display buffer
+
+		// Draw each snowflake:
+		for(f=0; f< NUMFLAKES; f++) {
+			display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+    		}
+
+		display.display(); // Show the display buffer on the screen
+		delay(200);        // Pause for 1/10 second
+
+		// Then update coordinates of each flake...
+		for(f=0; f< NUMFLAKES; f++) {
+			icons[f][YPOS] += icons[f][DELTAY];
+			// If snowflake is off the bottom of the screen...
+			if (icons[f][YPOS] >= display.height()) {
+				// Reinitialize to a random position, just off the top
+				icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+				icons[f][YPOS]   = -LOGO_HEIGHT;
+				icons[f][DELTAY] = random(1, 6);
+			}	
+		}
+	}
 }
